9cec087bdcde0766afd82e4c69746c9e5f5ccb8b
nrafolsom12 <nrafolsom12@gmail.com>
2023-12-02 13:02:58 -0600

    rewrote code

--- Day_2/Day2_pt1.py
+++ Day_2/Day2_pt1.py
@@ -1,56 +1,33 @@
 #!/usr/bin/python3
 
-# games possible if bag contains the following:
-# 12 red cubes
-# 13 green cubes
-# 14 blue cubes
-
-# Goal: identify possible games and get sum of game IDs
-
+import os
 import re
 
-# function to sum up arrays of each cube after converted to INTs
+input_path = os.path.join(os.path.dirname(__file__), "Day2_input.txt")
+input_file = open(input_path, "r")
+values = open(input_file).read().splitlines()
 
-def red_sum(arr):
-    red_total = 0
-    for num in arr:
-        red_total += num
-    return red_total
+r_cubes = 12
+g_cubes = 13
+b_cubes = 14
 
-def blue_sum(arr):
-    blue_total = 0
-    for num in arr:
-        blue_total += num
-    return blue_total
+re_pattern = r"\d+ (red|green|blue)"
+possible_games = 0 
 
-def green_sum(arr):
-    green_total = 0
-    for num in arr:
-        green_total += num
-    return green_total
+for game in values:
+    game_id = game.split(" ")[1][-1]
+    matches = [m.group() for m in re.finditer(re_pattern, game)]
+    is_possible = True
+    for match in matches:
+        (n, color) = match.split(" ")
+        if (
+            (color == "red" and int(n) > r_cubes)
+            or (color == "green" and int(n) > g_cubes)
+            or (color == "blue" and int(n) > b_cubes)
+        ) :
+            is_possible = False
+            break
+    if is_possible:
+        possible_games += int(game_id)
 
-
-test_string = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
-red = re.findall(r"(\d+)(?=\s*red)", test_string)
-red_int = []
-for i in range(len(red)):
-    t = int(red[i])
-    red_int.append(t)
-print(red_sum(red_int))
-
-blue = re.findall(r"(\d+)(?=\s*blue)", test_string)
-blue_int = []
-for i in range(len(blue)):
-    t = int(blue[i])
-    blue_int.append(t)
-print(blue_sum(blue_int))
-
-green = re.findall(r"(\d+)(?=\s*green)", test_string)
-green_int = []
-for i in range(len(green)):
-    t = int(green[i])
-    green_int.append(t)
-print(green_sum(green_int))
-
-# add logic to compare if game is possible
-
+print("Sum of possible games: ", possible_games)
