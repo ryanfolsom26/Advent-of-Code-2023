bbbdf6279c7afb024043d7f6035451fb21e68375
nrafolsom12 <nrafolsom12@gmail.com>
2024-01-12 23:14:37 -0600

    part 2 complete

---
+++ Day_4/2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+import re
+
+with open('Advent-of-Code-2023/Day_4/Day4_input.txt') as f:
+    input =  f.read()
+
+def totalCopies(input):
+    lines = input.split('\n')
+    regex = r':(.*)\|(.*)'
+    cards = [1] * len(lines)
+    for i, line in enumerate(lines):
+        win_nums, actual_nums = re.findall(regex, line)[0]
+        overlap = set(win_nums.split()) & set(actual_nums.split())
+        for j in range(len(overlap)):
+            cards[i+j+1] += cards[i]
+    
+    return sum(cards)
+
+print('Total: ', totalCopies(input))
